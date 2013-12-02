bf2f68b8de23b70d8640608cbcbc91f67c0bb5eb
UnovaChampionN <hamzashahid2002@hotmail.com>
2013-12-02 20:36:41 +0500

    Update pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -1,2 +1,2 @@
 exports.BattlePokedex = {
-venusaurmega:{num:3,species:"Venusaur-Mega",baseSpecies:"Venusaur",forme:"Mega",formeLetter:"M",types:["Grass","Poison"],baseStats:{hp:80,atk:100,def:123,spa:122,spd:120,spe:80},abilities:{0:"Thick Fat"},heightm:2.4,weightkg:155.5,color:"Green",prevo:"ivysaur",evoLevel:32,eggGroups:["Monster","Grass"]},
+venusaurmega:{num:1,species:"Venusaur-Mega",types:["Grass","Poison"],baseStats:{hp:80,atk:100,def:123,spa:122,spd:120,spe:80},abilities:{0:"Thick Fat"},heightm:2.4,weightkg:155.5,color:"Green",eggGroups:["Monster","Grass"]},
